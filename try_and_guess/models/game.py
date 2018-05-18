17e2f0efbd811e0ac68661b9c5eab640367d97c3
Federico Giraldi <fgiraldi@gmail.com>
2018-05-18 20:32:24 -0300

    Trying to remove Enums

--- try_and_guess/models/game.py
+++ try_and_guess/models/game.py
@@ -2,37 +2,41 @@ from django.db import models
 from enum import Enum
 
 
-class GuesserChoice(Enum):
-    USER = 'USER',
-    MACHINE = 'MACHINE'
-
-    def __str__(self):
-        return self.name
-
-
 class GameStatusChoice(Enum):
     START = 'START',
     USER_GUESSING = 'USER_GUESSING',
     MACHINE_GUESSING = 'MACHINE_GUESSING',
     FINISHED = 'FINISHED'
 
-    def __str__(self):
-        return self.name
+
+class GuesserChoice(Enum):
+    USER = 'USER',
+    MACHINE = 'MACHINE'
 
 
 class Game(models.Model):
+    GUESSER_CHOICES = (
+        ('USER', 'USER'),
+        ('MACHINE', 'MACHINE')
+    )
+    GAME_STATUS_CHOICES = (
+        ('START', 'START'),
+        ('USER_GUESSING', 'USER_GUESSING'),
+        ('MACHINE_GUESSING', 'MACHINE_GUESSING'),
+        ('FINISHED', 'FINISHED')
+    )
     status = models.CharField(
       max_length=25,
-      choices=[(tag, tag.value) for tag in GameStatusChoice],
-      default=GameStatusChoice.START
+      choices=GAME_STATUS_CHOICES,
+      default='START'
     )
     guess_number = models.IntegerField(default=0)
     range_min = models.IntegerField(default=1)
     range_max = models.IntegerField(default=100)
     guesser = models.CharField(
       max_length=15,
-      choices=[(tagg, tagg.value) for tagg in GuesserChoice],
-      default=GuesserChoice.USER
+      choices=GUESSER_CHOICES,
+      default='USER'
     )
 
     def __str__(self):
--- try_and_guess/views.py
+++ try_and_guess/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import Game
-from .models.game import GameStatusChoice, GuesserChoice
 from random import randint
 
 
@@ -13,18 +11,40 @@ def welcome(request):
 
 def user_starts(request):
     new_game = Game()
-    new_game.status = GameStatusChoice.START
-    new_game.guesser = GuesserChoice.USER
+    new_game.status = Game.GAME_STATUS_CHOICES['START']
+    new_game.guesser = Game.GUESSER_CHOICES['USER']
     new_game.save()
     new_game.guess_number = randint(new_game.range_min, new_game.range_max)
     new_game.save()
     context = {'game': new_game}
-    return HttpResponseRedirect(reverse('user_guesses', args=(new_game.id,)))
+    # return HttpResponseRedirect(reverse('user_guesses', args=(new_game.id,)))
+    return user_guesses(request, new_game.id)
 
 
 def user_guesses(request, game_id):
     game = get_object_or_404(Game, pk=game_id)
-    context = {'game': game}
+    if game.status == Game.GAME_STATUS_CHOICES['START']:
+        """ USER must start to guess"""
+        game.status = Game.GAME_STATUS_CHOICES['USER_GUESSING']
+        game.save()
+        clue = 'You have just started'
+    elif game.status == Game.GAME_STATUS_CHOICES['USER_GUESSING']:
+        """ USER has started to guess.
+        We must compare his number with the number to be guessed"""
+        try_number = request.POST['user_number']
+        # try_number = 0
+        if try_number == game.guess_number:
+            game.status = Game.GAME_STATUS_CHOICES['FINISHED']
+            game.save()
+            clue = 'You won'
+        elif try_number < game.guess_number:
+            clue = 'My number is bigger'
+        else:
+            clue = 'My number is smaller'
+    else:
+        clue = 'Estoy en cualquier estado %s' % game.status
+
+    context = {'game': game, 'clue': clue}
     return render(request, 'try_and_guess/user_guesses.html', context)
 
 
